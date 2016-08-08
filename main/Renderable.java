05a41d92392c8709d6cbc8f081da13c3c9080735
JosefUtbult <josef.utbult@hotmail.com>
2016-08-08 17:58:47 +0200

    Hej

---
+++ main/Renderable.java
@@ -0,0 +1,52 @@
+package main;
+
+import javafx.scene.transform.Affine;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+
+/**
+ * Created by josef on 2016-08-08.
+ */
+public abstract class Renderable {
+    private double posX;
+    private double posY;
+    private double height;
+    private double width;
+
+    public abstract void render(Graphics2D g);
+
+    public double getPosX(){
+        return this.posX;
+    }
+
+    public double getPosY(){
+        return this.posY;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setPosX(double X){
+        this.posX = X;
+    }
+
+    public void setPosY(double Y){
+        this.posY = Y;
+    }
+
+    public void setHeight(double tempHeight){
+        this.height = tempHeight;
+    }
+
+    public void setWidth(double tempWidth){
+        this.width = tempWidth;
+    }
+
+}
